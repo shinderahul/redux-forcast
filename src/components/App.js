c8e61b3b3f819deafddbcd997ddd25baa79805fe
Rahul Shinde <rahul.shinde@digitaslbi.com>
2018-01-14 17:03:41 +0530

    Added Search Bar container

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar';
 
 class App extends Component {
   render() {
     return (
       <div>
-          <h1>Bootstrap React Redux</h1>
+          <SearchBar />
       </div>
     );
   }
---
+++ src/containers/search_bar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	render() {
+		return (
+			<form className='input-group'>
+				<input
+					placeholder='Get a five-day forcast in your favorite cities'
+					className='form-control'
+				 />
+				 <span className='input-group-btn'>
+				 	<button type='submit' className='btn btn-secondary'>Submit</button>
+				 </span>
+			</form>
+		);
+	}
+}
+
+export default SearchBar;
