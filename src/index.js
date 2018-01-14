a498519863c834cef0585fd310dac1486257e349
Rahul Shinde <rahul.shinde@digitaslbi.com>
2018-01-14 16:20:16 +0530

    Bootstrap React Redux

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import './index.css';
+import App from './components/App';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware}>
+		<App />
+	</Provider>
+	, document.getElementById('root'));
