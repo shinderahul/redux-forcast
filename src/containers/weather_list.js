e07560b93fac0a353835bd58d992db1285858192
Rahul Shinde <rahul.shinde@digitaslbi.com>
2018-01-16 20:00:47 +0530

    Mapping props to a Render help

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,6 +3,16 @@ import { connect } from 'react-redux';
 
 
 class WeatherList extends Component {
+	renderWeather(cityData) {
+		const name = cityData.city.name;
+
+		return (
+			<tr key={name}>
+				<td>{name}</td>
+			</tr>
+		);
+	}
+
 	render() {
 		return (
 			<table className='table table-hover'>
@@ -15,6 +25,7 @@ class WeatherList extends Component {
 					</tr>
 				</thead>
 				<tbody>
+					{this.props.weather.map(this.renderWeather)}
 				</tbody>
 			</table>
 		);
